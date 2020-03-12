a56c073f486ad937414951c330bbd8fdd792629e
kwambokaB <paula.paulatek@gmail.com>
2020-03-12 10:38:08 +0300

    startup code

---
+++ elearn/app.js
@@ -0,0 +1,91 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const flash = require('connect-flash');
+const exphbs = require('express-handlebars');
+const expressValidator = require('express-validator');
+const session = require('express-session');
+const passport = require('passport');
+// const localStrategy = require('passport-local'), Strategy;
+const mongo = require('mongodb');
+const mongoose = require('mongoose')
+
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Express Session
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Express Validator
+app.use(expressValidator({
+errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+    , root    = namespace.shift()
+    , formParam = root;
+
+  while(namespace.length) {
+    formParam += '[' + namespace.shift() + ']';
+  }
+  return {
+    param : formParam,
+    msg   : msg,
+    value : value
+  };
+}
+}));
+
+// Connect-Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+res.locals.messages = require('express-messages')(req, res);
+next();
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
